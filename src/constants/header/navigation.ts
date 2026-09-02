39dec70acd9552258ed712897fd48aed517850ce
rodriknd <rp752989@gmail.com>
2026-09-02 11:42:44 -0400

    Feat: Implementando el Navbar header

---
+++ src/constants/header/navigation.ts
@@ -0,0 +1,30 @@
+export const NAVIGATION = [
+  {
+    label: 'Inicio',
+    href: '/',
+  },
+  {
+    label: 'Sobre',
+    href: '/about',
+  },
+  {
+    label: 'Libro',
+    href: '/book',
+  },
+  {
+    label: 'Gestión',
+    href: '/gestion',
+  },
+  {
+    label: 'Prensa',
+    href: '/prensa',
+  },
+  {
+    label: 'Agenda',
+    href: '/scheduler',
+  },
+  {
+    label: 'Buzón ciudadano',
+    href: '/buzon',
+  },
+];
